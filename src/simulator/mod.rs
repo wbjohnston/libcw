098b731abe5825d46e03717b315e9697e2ad0c44
Will Johnston <wbjohnston@gmail.com>
2017-08-28 17:23:49 -0400

    Removed non-existent module imports

--- src/simulator/mod.rs
+++ src/simulator/mod.rs
@@ -3,12 +3,6 @@
 mod simulator_builder;
 pub use self::simulator_builder::SimulatorBuilder;
 
-mod error;
-pub use self::error::SimulatorError;
-
-mod event;
-pub use self::event::SimulatorEvent;
-
 mod simulator;
 pub use self::simulator::Simulator;
 
